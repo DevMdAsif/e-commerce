f5ca7390aba139a0c0bc633c9eaa98e2bb94db54
MD.Asif Ullah <mdasifullah456@gmail.com>
2024-03-24 23:30:04 +0600

    create user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,14 +1,50 @@
-const users = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "john@gmail.com",
-    },
-    {
-        id: 2,
-        name: "asifullah",
-        email: "mdasifullah@gmail.com",
-    },
-];
+import { Schema, model } from "mongoose";
+import { hashSync, compareSync } from "bcrypt";
+import { defaultImage } from "../serect.js";
 
-export default users;
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            trim: true,
+            minlangth: [3, "Name must be at least 3 characters"],
+            maxlength: [20, "Name must be at most 20 characters"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: [true, "Email already exists"],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please enter a valid e-mail address",
+            ],
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            match: [
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$",
+                "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character",
+            ],
+            Set: (value) => hashSync(value, 30),
+        },
+        image: {
+            type: String,
+            default: defaultImage,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        isBanned: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const user = model("user", userSchema);
+
+export default user;
--- src/serect.js
+++ src/serect.js
@@ -1,8 +1,10 @@
 const port = process.env.SERVER_PORT;
 
+const defaultImage = "public/images/users/profileImg.jpg";
+
 // connect to MongoDB
 
 const MONGODB_URL =
     process.env.MONGODBATLAS_URL || "mongodb://localhost:27017/e-commerce";
 
-export { port, MONGODB_URL };
+export { port, MONGODB_URL, defaultImage };
